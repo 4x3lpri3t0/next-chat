e61ec087cfe0d26a13c1988c9ef68f6a1c1336d4
Axel Prieto <prietoaxel@gmail.com>
2021-11-25 20:54:42 +0000

    Add OfflineDirecLine

---
+++ OfflineDirectLine/index.js
@@ -0,0 +1,5 @@
+const directline = require("../dist/bridge");
+const express = require("express");
+
+const app = express();
+directline.initializeRoutes(app, 3000, "http://localhost:3978/api/messages");
